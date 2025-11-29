a6bf908fcd8ec2aa5cd891208e6f98e528c6faf1
SimbaMupfu <scmupfururirwa@gmail.com>
2025-11-29 19:06:51 +0200

    Add controller to handle client requests.

--- src/main/java/inc/hustles/sims/controller/BookFinderRestController.java
+++ src/main/java/inc/hustles/sims/controller/BookFinderRestController.java
@@ -1,19 +1,133 @@
 package inc.hustles.sims.controller;
 
 import inc.hustles.sims.model.Book;
+import inc.hustles.sims.model.BorrowingRecord;
+import inc.hustles.sims.model.Member;
+import inc.hustles.sims.service.LibraryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/api")
 public class BookFinderRestController {
 
     private static final Logger logger = LoggerFactory.getLogger(BookFinderRestController.class);
 
-    @GetMapping("book")
-    public Book getBook(){
+    @Autowired
+    private LibraryService libraryService;
+
+    @GetMapping("/books")
+    public ResponseEntity<List<Book>> getBook(){
+        List<Book> books = libraryService.getAllBooks();
         logger.info("getBook() method called");
-        return new Book("Sims", "The stories of Hustling in the streets");
+        return new ResponseEntity<>(books, HttpStatus.OK);
+    }
+
+    @GetMapping("/books/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id){
+        Optional<Book> book = libraryService.getBookById(id);
+        logger.info("The book returned " + book);
+        return book.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/books")
+    public ResponseEntity<Book> addBook(@RequestBody Book book){
+        libraryService.addBook(book);
+        logger.info("The book was added " + book);
+        return new ResponseEntity<>(book, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/books/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book updatedBook){
+        if(!libraryService.getBookById(id).isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        updatedBook.setId(id);
+        libraryService.updateBook(updatedBook);
+        logger.info("The book has been updated " + updatedBook);
+        return new ResponseEntity<>(updatedBook, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/books/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        if (!libraryService.getBookById(id).isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        libraryService.deleteBook(id);
+        logger.info("The book has been deleted ");
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/members")
+    public ResponseEntity<List<Member>> getAllMembers() {
+        List<Member> members = libraryService.getAllMembers();
+        logger.info("The members in the system " + members);
+        return new ResponseEntity<>(members, HttpStatus.OK);
+    }
+
+    @GetMapping("/members/{id}")
+    public ResponseEntity<Member> getMemberById(@PathVariable Long id) {
+        Optional<Member> member = libraryService.getMemberById(id);
+        logger.info("The member you retrieved " + member);
+        return member.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/members")
+    public ResponseEntity<Member> addMember(@RequestBody Member member) {
+        libraryService.addMember(member);
+        logger.info("The member has been added " + member);
+        return new ResponseEntity<>(member, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/members/{id}")
+    public ResponseEntity<Member> updateMember(@PathVariable Long id, @RequestBody Member updatedMember) {
+        if (!libraryService.getMemberById(id).isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        updatedMember.setId(id);
+        libraryService.updateMember(updatedMember);
+        logger.info("The member has been updated " + updatedMember);
+        return new ResponseEntity<>(updatedMember, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/members/{id}")
+    public ResponseEntity<Void> deleteMember(@PathVariable Long id) {
+        if (!libraryService.getMemberById(id).isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        libraryService.deleteMember(id);
+        logger.info("The member has been deleted " + id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/borrowing-records")
+    public ResponseEntity<List<BorrowingRecord>> getAllBorrowingRecords() {
+        List<BorrowingRecord> records = libraryService.getAllBorrowingRecords();
+        logger.info("The records has been retrieved " + records);
+        return new ResponseEntity<>(records, HttpStatus.OK);
+    }
+
+    @PostMapping("/borrow")
+    public ResponseEntity<BorrowingRecord> borrowBook(@RequestBody BorrowingRecord record) {
+        libraryService.borrowBook(record);
+        logger.info("The book has been borrowed " + record);
+        return new ResponseEntity<>(record, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/return/{recordId}")
+    public ResponseEntity<Void> returnBook(@PathVariable Long recordId) {
+        libraryService.returnBook(recordId, LocalDate.now());
+        logger.info("The book has been retrieved " + recordId);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
